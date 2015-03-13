337f96c2867f2f4a70b993426337fcacea35a2cf
zuoshifan <zuoshifan@163.com>
2015-03-13 20:44:16 +0800

    Initialized
    Some pyhton files from driftscan package.

---
+++ README.rst
@@ -0,0 +1,7 @@
+instimager
+==========
+
+Instantaneous imager for FFT telescope
+
+
+Package to make instantaneous (short integration time) sky map for a FFT (Fast Fourier Transform) telescope array. The map-making method is based on Tegmark & Zaldarriaga, 2009, arXiv:astro-ph 0805.4414.
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name = 'instimager',
+    version = 0.1,
+
+    packages = find_packages(),
+    # scripts=['scripts/caput-pipeline'],
+    requires = ['numpy', 'h5py', 'healpy'],  # Probably should change this.
+
+    # metadata for upload to PyPI
+    author = "Shifan Zuo",
+    author_email = "sfzuo@bao.ac.cn",
+    description = "Instantaneous imager for FFT telescope.",
+    license = "GPL v3.0",
+    url = "http://github.com/zuoshifna/instimager"
+)
